8917d6ab78d94defc6cdc3273049557f4ae4ae3f
“Schilings” <1146830743@qq.com>
2022-10-01 19:08:27 +0800

    0.0.1

---
+++ neiko-kubernetes/neiko-kubernetes-modules/neiko-kubernetes-upms/src/main/java/com/schilings/neiko/k8s/system/UpmsApplication.java
@@ -0,0 +1,29 @@
+package com.schilings.neiko.k8s.system;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.TimeUnit;
+
+@EnableScheduling
+@EnableDiscoveryClient(autoRegister = false)
+@SpringBootApplication
+public class UpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpmsApplication.class);
+	}
+
+	@Value("${dummy.message}")
+	private String value;
+
+	@Scheduled(fixedDelay = 5, timeUnit = TimeUnit.SECONDS)
+	public void sout() {
+		System.out.println(value);
+	}
+
+}
